f264b6aad39b11b75bff07d42dd60cfe1dd8a996
Sergio Torres <sergio.torres@idr.edu.mx>
2025-06-12 13:40:46 -0600

    feat: patrón y antipatrón

--- java10_encapsulacion/src/com/santander/Atm.java
+++ java10_encapsulacion/src/com/santander/Atm.java
@@ -30,6 +30,63 @@ public class Atm {
 	private String model;
 	private String address;
 	
+	/* Métodos constructores
+	 *  Un constructor es un tipo especial de método que se llama automáticamente
+	 *  cuando se crea una instancia(objeto) de una clase. Su propósito principal
+	 *  es inicializar el estado de un objeto recién creado.
+	 *  
+	 *  Reglas:
+	 *  - El nombre del constructor debe ser el mismo que el de la clase.
+	 *  - Un constructor no tiene retorno.
+	 *  - Un constructor puede llamar a otro constructor de la misma clase usando
+	 *  	la palabra this(). Esta llamada debe ser la primera instrucción del constructor.
+	 *  - Es posible tener una sobrecar de constructores.
+	 *  - Constructor por default: Si no se proporciona un constructor en la clase, Java
+	 *  	proporciona un constructor por defecto sin parámetros.
+	 *    <ModificarDeAcceso> NombreDeLaClase(parámetros para inicializar atributos)
+	 */
+	public Atm() {
+		this(1,500_000, "T-800", "n/a");
+		/*this.serialNumber = 1;
+		this.balance = 500_000;
+		this.model = "T-800";
+		this.address = "n/a";*/
+	}
+	/*
+	 * El Telescoping Constructor Anti-pattern (Antipatrón del 
+	 * Constructor Telescópico) en Java ocurre cuando una clase 
+	 * tiene múltiples constructores, y cada nuevo constructor 
+	 * toma los parámetros de otro y añade uno nuevo.
+	 */
+	public Atm(double balance) {
+		this(1, balance, "T-800", "n/a");
+	}
+	
+	public Atm(int serialNumber, double balance,String model, String address) {
+		this.serialNumber = serialNumber;
+		this.balance = balance;
+		this.model = model;
+		this.address = address.toUpperCase();
+	}
+	
+	/**
+	 *  factory method
+	 *  https://refactoring.guru/es/design-patterns
+	 *  Los patrones de diseño creacionales se utilizan para promover 
+	 *  el código limpio, modular y fácil de mantener, así como para 
+	 *  mejorar la flexibilidad y la reutilización del código.
+	 *  
+	 *  Patrón de diseño: Es una solución reutilizable y 
+	 *  probada para problemas comunes en el desarrollo de software.
+	 *  Anti-patrón: Es una práctica de diseño que parece 
+	 *  útil pero que en realidad genera problemas de 
+	 *  mantenimiento, rendimiento o escalabilidad.
+	 */
+	public static Atm createAirPortAtm() {
+		return new Atm(1000, 900_000, "T-2000", "Airport");
+	}
+	
+	
 	/*
 	 * Para acceder a los atributos encapsulados, usamos
 	 * los métodos Setters y Getters.
@@ -53,5 +110,32 @@ public class Atm {
 		this.balance = balance;		
 	}
 	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	// Realizar los setters y getters para model y address
+	public void setModel(String model) {
+	    this.model = model;
+	}
+
+	public String getModel() {
+	    return this.model;
+	}
+
+	public void setAddress(String address) {
+	    this.address = address;
+	}
+
+	public String getAddress() {
+	    return this.address;
+	}
+	
+	public String toString() {
+		return "Serial Number: " + this.serialNumber +
+				" Balance: " + this.balance +
+				" Model: " + this.model +
+				" Address: " + this.address;
+	}
 
 }
--- java10_encapsulacion/src/com/santander/AtmTest.java
+++ java10_encapsulacion/src/com/santander/AtmTest.java
@@ -3,7 +3,20 @@ package com.santander;
 public class AtmTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Atm cajero01 = new Atm();
+		cajero01.setSerialNumber(1);
+		cajero01.setBalance(500_000);
+		cajero01.setModel("T-800");
+		cajero01.setAddress("Av. siempre viva");
+		
+		Atm cajero02 = new Atm(2, 400_00, "T-1000", "Av. Concha 8254");
+		Atm cajero03 = new Atm();
+		Atm cajero04 = new Atm(100_000);
+		
+		System.out.println( cajero01 );
+		System.out.println( cajero02 );
+		System.out.println( cajero03 );
+		System.out.println( cajero04 );
 
 	}
 
