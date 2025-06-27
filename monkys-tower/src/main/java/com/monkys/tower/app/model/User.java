bb5dc0816af19f405e5ff45cbee21b6f61577426
Sergio Torres <sergio.torres@idr.edu.mx>
2025-06-27 12:46:06 -0600

    feat: configuración del perfil para mySQL

--- monkys-tower/src/main/java/com/monkys/tower/app/model/User.java
+++ monkys-tower/src/main/java/com/monkys/tower/app/model/User.java
@@ -1,6 +1,10 @@
 package com.monkys.tower.app.model;
 
 import java.time.*;
+import java.util.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.*;
 
@@ -11,17 +15,35 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+	@Column(name = "first_name", length = 50, nullable = false)
 	private String firstName;
-	
+	@Column(name = "last_name", length = 50, nullable = false)
 	private String lastName;
-	
+	@Column(name = "email", length = 100, nullable = false, unique = true)
 	private String email;
-	
+	@Column(name = "password", length = 255, nullable = false)
 	private String password;
-	
+	@Column(name = "phone_number", length = 20, nullable = true)
 	private String phoneNumber;
-	
+	@Column(name = "birth_date", nullable = true)
 	private LocalDate birthDate;
+	@Column(name = "avatar_url", length = 255, nullable = true)
+	private String avatarUrl;
+	@Column(name = "is_active", nullable = false)
+	private boolean active = true;
+	
+	// Hibernate asignará la fecha y hora de creación automáticamente
+	// updatable/insertable : el campo no debe ser modificado manualmente
+	//                    desde el código, dejando que Hibernate la gestione.
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	// Hibernate asignará la fecha y hora de actualización automáticamente
+	@UpdateTimestamp
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
 
+	@ManyToMany
+	private Set<Role> roles = new HashSet<>();
+	
 }
